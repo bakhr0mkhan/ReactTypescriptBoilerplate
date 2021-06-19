1907a52430d8f9dbe648d5bbeddffacce8b9023a
Bakhromkhan <67605887+bakhr0mkhan@users.noreply.github.com>
2021-06-19 15:05:07 +0500

    implemented redux

--- src/Screens/Dashboard/index.tsx
+++ src/Screens/Dashboard/index.tsx
@@ -1,14 +1,24 @@
 import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// redux actions
+import { increment, decrement } from "../../state/actions";
 
 interface Props {}
 
 const Dashboard = (props: Props) => {
+  const counter = useSelector((state) => state);
+  // hooks
+  const dispatch = useDispatch();
+  // effects
   useEffect(() => {
     console.log("DOTENV", process.env.REACT_APP_ENV_CHECK);
   }, []);
   return (
     <div>
-      <div>Dashboard</div>
+      <div>Counter {counter}</div>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import counterReducer from "./state/reducers";
+
+import { Provider } from "react-redux";
+
+// store
+let store = createStore(counterReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
